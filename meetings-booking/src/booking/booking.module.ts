a2a6b923b43fb906ee5e14b4cb6cab301b6540bf
cntrlshiftdelete <74133785+cntrlshiftdelete@users.noreply.github.com>
2025-11-08 12:33:29 +0530

    :construction: Repository creation

--- meetings-booking/src/booking/booking.module.ts
+++ meetings-booking/src/booking/booking.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { BookingController } from './booking.controller';
 import { BookingService } from './services/booking.service';
 import { RecurrenceService } from './services/recurrence.service';
+import { BookingSeries } from './entities/booking-series.entity';
+import { BookingInstance } from './entities/booking-instance.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [BookingController],
   providers: [BookingService, RecurrenceService],
   exports: [BookingService],
+  imports: [TypeOrmModule.forFeature([BookingSeries, BookingInstance])],
 })
 export class BookingModule {}
